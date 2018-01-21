1648fc48fcb3321888be67af713a9a96707c8824
mattlucian <matt@inventorysource.com>
2018-01-20 21:02:53 -0500

    modified quotes

--- flask-example-1.py
+++ flask-example-1.py
@@ -14,10 +14,10 @@ def test(name):
     quotes = [
         "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
         "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ",
-        "'To understand recursion you must first understand recursion..' -- Unknown",
-        "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- Unknown",
+        "'To understand recursion you must first understand recursion..' -- Whoah Dude",
+        "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- George Washington",
         "'Mathematics is the key and door to the sciences.' -- Galileo Galilei",
-        "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Unknown"]
+        "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Abraham Lincoln"]
     randomNumber = randint(0, len(quotes) - 1)
     quote = quotes[randomNumber]
 
@@ -27,4 +27,4 @@ def test(name):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=8080)
